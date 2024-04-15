b65510e9c14dbb15e88aeff1b3e95d585849cedb
magnitopic <magnitopic@gmail.com>
2024-04-15 09:24:18 +0200

    setup postgresql in docker and connecting to webpage

--- src/backend/game/views.py
+++ src/backend/game/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-	return HttpResponse("<h1>Hello, World!</h1>")
+
+
+def hello(req):
+    return HttpResponse("<h1>Hello, World!</h1>")
+
+
+def about(req):
+    return HttpResponse("<h1>About</h1>")
