7f294a2eedae23000be5998e22fa4363ad6d20d1
magnitopic <magnitopic@gmail.com>
2024-04-22 13:16:04 +0200

    api updates user values

--- src/backend/users/api.py
+++ src/backend/users/api.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from ninja import NinjaAPI
 from game.models import User
-from .schema import UserSchema, UserCreateSchema
+from .schema import UserSchema, UserCreateSchema, Error, UserUpdateSchema
 
 app = NinjaAPI()
 
@@ -17,8 +17,22 @@ def get_user(request, user_id: int):
     return user
 
 
-@app.post("", response=UserSchema)
+@app.post("", response={200: UserSchema, 400: Error})
 def create_user(request, user_in: UserCreateSchema):
+    if User.objects.filter(name=user_in.name).exists():
+        return 400, {"msg": "User already exists"}
+
     user_data = user_in.model_dump()
     user_model = User.objects.create(**user_data)
     return user_model
+
+
+@app.post("/{user_id}/update", response={200: UserSchema, 400: Error})
+def update_user(request, user_id: int, user_in: UserUpdateSchema):
+    user = get_object_or_404(User, userID=user_id)
+    user_data = user_in.dict()
+    for key, value in user_data.items():
+        if value is not None:
+            setattr(user, key, value)
+    user.save()
+    return user
--- src/backend/users/schema.py
+++ src/backend/users/schema.py
@@ -23,3 +23,24 @@ class UserSchema(ModelSchema):
 class UserCreateSchema(Schema):
     name: str
     password: str
+    profilePicture: str = ""
+    totalPoints: int = 0
+    status: bool = 0
+    matchesTotal: int = 0
+    matchesWon: int = 0
+    matchesLost: int = 0
+    matchesDraw: int = 0
+
+class UserUpdateSchema(Schema):
+    password: str = None
+    profilePicture: str = None
+    totalPoints: int = None
+    status: bool = None
+    matchesTotal: int = None
+    matchesWon: int = None
+    matchesLost: int = None
+    matchesDraw: int = None
+
+
+class Error(Schema):
+    msg: str
