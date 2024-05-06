85c9d82f95c999d6f7a2ad3f932bf0b0fab6a2c8
root <root@LAPTOP-UTKVD2O1>
2024-05-06 18:11:16 +0200

    auth fetch

--- src/frontend/js/auth.js
+++ src/frontend/js/auth.js
@@ -6,7 +6,7 @@
 /*   By: adpachec <adpachec@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 11:27:22 by adpachec          #+#    #+#             */
-/*   Updated: 2024/04/22 18:14:03 by adpachec         ###   ########.fr       */
+/*   Updated: 2024/05/06 18:11:01 by adpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,50 @@ const users = [
     { username: 'user3', password: 'pass3' }
 ];
 
-function login(username, password)
-{
-    const user = users.find(user => user.username === username && user.password === password);
-    if (user)
-    {
-        const token = btoa(username + ':' + password);
-        localStorage.setItem('userToken', token);
-        return true;
-    }
-    else
-    {
+// function login(username, password)
+// {
+//     const user = users.find(user => user.username === username && user.password === password);
+//     if (user)
+//     {
+//         const token = btoa(username + ':' + password);
+//         localStorage.setItem('userToken', token);
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+
+async function login(username, password) {
+    const loginEndpoint = 'http://localhost:8000/api/auth/login';
+    
+    try {
+        const response = await fetch(loginEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+        });
+        
+        if (response.ok) {
+            const token = username;
+            localStorage.setItem('userToken', token);
+            updateNavbar();
+            router.route('/profile');
+            return true;
+        } else {
+            throw new Error('Login failed');
+        }
+    } catch (error) {
+        console.error('An error occurred during login:', error);
         return false;
     }
+    return false;
 }
 
 function isLoggedIn()
@@ -39,11 +70,36 @@ function isLoggedIn()
     return localStorage.getItem('userToken') ? true : false;
 }
 
-function logout()
-{
-    localStorage.removeItem('userToken');
-    updateNavbar();
-	router.route('/home');
+// function logout()
+
+// {
+//     localStorage.removeItem('userToken');
+//     updateNavbar();
+// 	router.route('/home');
+// }
+
+async function logout() {
+    const logoutEndpoint = 'http://localhost:8000/api/auth/logout';
+    try {
+        const response = await fetch(logoutEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('userToken')}`
+            },
+        });
+
+        if (response.ok) {
+            localStorage.removeItem('userToken');
+            updateNavbar();
+            router.route('/home');
+            console.log('Logged out successfully.');
+        } else {
+            throw new Error('Logout failed');
+        }
+    } catch (error) {
+        console.error('An error occurred during logout:', error);
+    }
 }
 
 function getUsernameFromToken() {
